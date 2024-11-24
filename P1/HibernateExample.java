bce7aed0fee64df1f2ea0a4aadd15f82bd086358
JustAFailure <68466522+JustAFailuree@users.noreply.github.com>
2024-11-24 11:40:17 +0100

    Update HibernateExample.java

--- P1/HibernateExample.java
+++ P1/HibernateExample.java
@@ -5,16 +5,16 @@
 package com.mycompany.hibernateexample;
 
 import com.example.entity.User; 
-
 import org.hibernate.Session; 
-
 import org.hibernate.SessionFactory; 
-
 import org.hibernate.Transaction; 
-
 import org.hibernate.cfg.Configuration; 
 
- 
+
+/**
+ *
+ * @author Radek
+ */
 
 public class HibernateExample { 
 
